f27c3d1f8191b8f88c39aa94bbb9212432597a93
MiltonMartinez29 <alfredoxd321@gmail.com>
2022-10-16 22:54:35 -0600

    first commit

---
+++ Foro1_ejercicio2.js
@@ -0,0 +1,11 @@
+let Edad=21;
+console.log("Ingrese Su Edad:  "+Edad)
+
+
+
+    if(Edad>=18){
+       console.log("Puedes Conducir ");
+    }
+    else {
+    console.log("No Puedes Conducir");
+    }
